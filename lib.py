1344bff8a5569fc9a26d05074f294365a63bdf15
Iusearchonmobile <166061410+Iusearchonmobile@users.noreply.github.com>
2025-12-05 20:59:41 +0300

    Update lib.py

--- lib.py
+++ lib.py
@@ -2,7 +2,7 @@ import requests
 import uuid
 import json
 
-def getrand(keyword, plrsunder, excluisons=[]):
+def getrand(keyword: str, plrsunder: int, plrsover: int, excluisons=[]):
     url = 'https://apis.roblox.com/search-api/omni-search'
     session = str(uuid.uuid4())
     token = None
@@ -23,7 +23,7 @@ def getrand(keyword, plrsunder, excluisons=[]):
             for group in res["searchResults"]:
                 if "contents" in group:
                     for game in group["contents"]:
-                        if game.get("playerCount", 0) <= plrsunder and game.get("rootPlaceId") not in excluisons:
+                        if game.get("playerCount", 0) <= plrsunder and game.get("playerCount", 0) >= plrsover and game.get("rootPlaceId") not in excluisons:
                             imageurljson = requests.get(f"https://thumbnails.roblox.com/v1/games/icons?universeIds={game.get('universeId')}&size=150x150&format=Png&isCircular=false").json()
                             image_url = imageurljson["data"][0]["imageUrl"]
                             return {
@@ -43,12 +43,3 @@ def getrand(keyword, plrsunder, excluisons=[]):
         token = res.get("nextPageToken")
         if not token:
             return None
-
-if __name__ == "__main__":
-    res = getrand('\\n', 5, excluisons=json.loads(open('exclude.json', 'r').read()))
-
-    exclusions = json.load(open('exclude.json', 'r'))
-
-    with open('exclude.json', 'w') as outfile:
-        exclusions.append(res['placeid'])
-        json.dump(exclusions, outfile, indent=4)
